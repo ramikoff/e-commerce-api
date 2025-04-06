991ab1ad040860b54a5d444d24840407dc1cd7fd
ramil_novruzov <ramil.novruzov@gmx.de>
2025-04-06 18:10:52 +0200

    fixed category

--- models/Category.js
+++ models/Category.js
@@ -1,27 +1,8 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../db/index.js";
 
-<<<<<<< HEAD:models/User.js
-const User = sequelize.define("User", {
+const Category = sequelize.define("Category", {
   name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false },
-=======
-const Category = sequelize.define('Category', {
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  }
->>>>>>> main:models/Category.js
 });
 
 export default Category;
